f1d16e7b420da1439fc15899a7e23c2ce4cfb9f8
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-09-01 11:28:39 +0300

    исправлено имя пина

--- source/js/script.js
+++ source/js/script.js
@@ -19,7 +19,7 @@ ymaps.ready(function () {
       // Необходимо указать данный тип макета.
       iconLayout: 'default#image',
       // Своё изображение иконки метки.
-      iconImageHref: '../img/icon-map-pin.svg',
+      iconImageHref: 'img/icon-map-pin.svg',
       // Размеры метки.
       iconImageSize: [67, 100],
       // Смещение левого верхнего угла иконки относительно
