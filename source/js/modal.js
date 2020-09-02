fb33794cb4568873bb839d2f46ef1ce9e7ab1346
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-09-02 18:12:33 +0300

    Добавлено появление модального окна в index

---
+++ source/js/modal.js
@@ -0,0 +1,25 @@
+const weekProductBtn = document.querySelector('.week-product__btn');
+const modalOrder = document.querySelector('.modal-order');
+const modalOrderWindow = document.querySelector('.modal-order__window');
+
+weekProductBtn.addEventListener('click', function (evt) {
+  modalOrder.classList.add('modal-order--open');
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modalOrder.classList.contains("modal-order--open")) {
+      evt.preventDefault();
+      modalOrder.classList.remove("modal-order--open");
+    }
+  }
+});
+
+document.addEventListener('click', function (evt) {
+  if (!modalOrderWindow.contains(evt.target) && !weekProductBtn.contains(evt.target)) {
+    if (modalOrder.classList.contains("modal-order--open")) {
+      evt.preventDefault();
+      modalOrder.classList.remove("modal-order--open");
+    }
+  }
+});
